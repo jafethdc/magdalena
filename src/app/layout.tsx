6e527e1a50b1c11cf7338095bd33d3991fd39ac2
Irwin Eguiluz <ijea92@gmail.com>
2024-12-03 11:49:13 -0500

    remove redundant classes, use Radix components and move back ClientProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Grid } from "@radix-ui/themes";
+import { Grid, Box } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import ClientProviders from "./ui/client-providers";
 import "tailwindcss/tailwind.css";
@@ -20,19 +20,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Grid className="grid min-h-screen grid-rows-[auto,1fr,auto]">
-          <div className="row-start-1 row-end-2">
-            <Header />
-          </div>
-          <div className="row-start-2 row-end-3 overflow-auto">
-            <div className="mx-auto max-w-7xl px-4 py-8">
-              <ClientProviders>{children}</ClientProviders>
-            </div>
-          </div>
-          <div className="row-start-3 row-end-4">
-            <Footer />
-          </div>
-        </Grid>
+        <ClientProviders>
+          <Grid className="min-h-screen grid-rows-[auto,1fr,auto]">
+            <Box className="row-start-1 row-end-2">
+              <Header />
+            </Box>
+            <Box className="row-start-2 row-end-3 overflow-auto">
+              <Box className="mx-auto max-w-7xl px-4 py-8">
+                {children}
+              </Box>
+            </Box>
+            <Box className="row-start-3 row-end-4">
+              <Footer />
+            </Box>
+          </Grid>
+        </ClientProviders>
       </body>
     </html>
   );
